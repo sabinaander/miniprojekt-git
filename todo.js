c532d8653f9179c3063fd48118ae995aa40d2344
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2021-12-09 16:16:43 +0100

    Added form and button to form Fixes #2

--- todo.js
+++ todo.js
@@ -13,10 +13,58 @@ toDoBtn.innerHTML = 'ADD TO-DO'
 // appending toDoBtn to divs
 sideBar.appendChild(toDoBtn)
 
-// create div
+// create div container for todos/todo/edit todo
 toDoBtn.onclick = function () {
+    // if (!toDoContainer){
     let toDoContainer = document.createElement('DIV')
-    toDoContainer.innerHTML = 'ADD TO-DO'
+    toDoContainer.classList.add('toDoContainer')
+    // toDoContainer.innerHTML = 'ADD TO-DO'
+    document.body.appendChild(toDoContainer)
     console.log (toDoContainer)
+
+    // toDoForm = function () {
+
+        // create CLOSE button and add text
+        
+
+        let toDoForm = document.createElement('FORM')
+        toDoForm.setAttribute('id', 'toDoForm')
+        toDoContainer.appendChild(toDoForm)
+
+        // let closeContainerBtn = document.createElement('BUTTON')
+        // submitToDoBtn.innerHTML = 'CLOSE'
+        // document.getElementById('toDoForm').appendChild(closeContainerBtn)
+
+        let toDoInput = document.createElement('INPUT')
+        toDoInput.setAttribute('type','text')
+        toDoForm.innerHTML = ''
+        document.getElementById('toDoForm').appendChild(toDoInput)
+
+        // create button and add text
+        let submitToDoBtn = document.createElement('BUTTON')
+        submitToDoBtn.innerHTML = 'SUBMIT'
+        document.getElementById('toDoForm').appendChild(submitToDoBtn)
+    //  }
  }
 
+ 
+
+
+    
+//     toDoContainer = document.createElement('')
+    
+// }
+
+
+// Stuff that comes from teacher..
+
+// create a todo
+//  const toDo = [{
+//     title: '',
+//     text: '',
+//     date: '',
+//  }]
+
+ // Display all todos for a date
+
+//  const dailyToDo = toDo.filter({toDo} == toDo.date === 'date')
