ff0b980d8bc17dcbdc59bc64b634a64b14885c9b
Tomas Fridekrans <vindicated@hotmail.com>
2021-12-13 15:38:44 +0100

    see number of todos per day in calendar Fixes #35

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 window.addEventListener("load", main)
 
-const toDos = []
+/**
+ * @type {Array<{ title: String, description: String, date: String }>} asdas
+ */
+const toDos = [
+  {
+    title: "test todo1",
+    description: "some text",
+    date: "2021-12-01",
+  },
+  {
+    title: "test todo1",
+    description: "some text",
+    date: "2021-12-13",
+  },
+]
 
 // Starts the program
 function main() {
