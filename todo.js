899325adf1e5efdcf8aa06bcbb7d53c932fd0261
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2021-12-14 11:11:37 +0100

    commit

--- todo.js
+++ todo.js
@@ -4,19 +4,19 @@
 // View the to-do's in calendar
 
 // getting the sideBar
-let sideBar = document.querySelector(".sideBar")
+let sideBar = document.querySelector('.sideBar')
 
 // create button and add text
-let toDoBtn = document.createElement("BUTTON")
-toDoBtn.innerHTML = "ADD TO-DO"
+let toDoBtn = document.createElement('BUTTON')
+toDoBtn.innerHTML = 'ADD TO-DO'
 
 // appending toDoBtn to divs
 sideBar.appendChild(toDoBtn)
 
 // create div container for todos/todo/edit todo
 toDoBtn.onclick = function (event) {
-    
-  // if (!toDoContainer){
+
+    // if (!toDoContainer){
     let toDoContainer = document.createElement('DIV')
     toDoContainer.classList.add('toDoContainer')
     // toDoContainer.innerHTML = 'ADD TO-DO'
@@ -28,46 +28,50 @@ toDoBtn.onclick = function (event) {
     toDoContainer.appendChild(toDoForm)
 
 
-        // create CLOSE button and add text
-        // let closeContainerBtn = document.createElement('BUTTON')
-        // closeContainerBtn.innerHTML = 'CLOSE'
-        // toDoForm.appendChild(closeContainerBtn)
+    // create CLOSE button and add text
+    // let closeContainerBtn = document.createElement('BUTTON')
+    // closeContainerBtn.innerHTML = 'CLOSE'
+    // toDoForm.appendChild(closeContainerBtn)
 
-        let titleInput = document.createElement('INPUT')
-        titleInput.classList.add('titleInput')
-        titleInput.setAttribute('type', 'text')
-        titleInput.setAttribute('name', 'title')
-        toDoForm.appendChild(titleInput)
+    let titleInput = document.createElement('INPUT')
+    titleInput.classList.add('titleInput')
+    titleInput.setAttribute('type', 'text')
+    titleInput.setAttribute('name', 'title')
+    toDoForm.appendChild(titleInput)
 
-        // create div element for big input with description of the todo
-        let descriptionInput = document.createElement('INPUT')
-        descriptionInput.classList.add('descriptionInput')
-        descriptionInput.setAttribute('type', 'text')
-        descriptionInput.setAttribute('name', 'description')
-        toDoForm.appendChild(descriptionInput)
+    // create div element for big input with description of the todo
+    let descriptionInput = document.createElement('INPUT')
+    descriptionInput.classList.add('descriptionInput')
+    descriptionInput.setAttribute('type', 'text')
+    descriptionInput.setAttribute('name', 'description')
+    toDoForm.appendChild(descriptionInput)
+
+
+    let dateInput = document.createElement('INPUT')
+    dateInput.classList.add('dateInput')
+    dateInput.setAttribute('type', 'date')
+    dateInput.setAttribute('name', 'date')
+    toDoForm.appendChild(dateInput)
+
+    // create button and add text
+    let submitToDoBtn = document.createElement('BUTTON')
+
+    submitToDoBtn.innerHTML = 'SUBMIT'
+    toDoForm.appendChild(submitToDoBtn)
+
+    toDoForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        const formData = new FormData(event.target)
+        const toDo = Object.fromEntries(formData)
+        toDos.push(toDo)
 
         
-        let dateInput = document.createElement('INPUT')
-        dateInput.classList.add('dateInput')
-        dateInput.setAttribute('type', 'date')
-        dateInput.setAttribute('name', 'date')
-        toDoForm.appendChild(dateInput)
 
-        // create button and add text
-        let submitToDoBtn = document.createElement('BUTTON')
+        //  localstorage
 
-        submitToDoBtn.innerHTML = 'SUBMIT'
-        toDoForm.appendChild(submitToDoBtn)
-
-        toDoForm.addEventListener('submit', (event) => {
-            event.preventDefault()
-
-    const formData = new FormData(event.target)
-    const toDo = Object.fromEntries(formData)
-    toDos.push(toDo)
-
-    console.log(toDos)
-  })
+        console.log(toDos)
+    })
 }
 
 // }
