529edba189c492bcfd30169807172b15fab33863
Jim Bothén <jim.bothen@gmail.com>
2021-12-14 13:27:24 +0100

    Get clock to header Fixes #37

--- JS/today.js
+++ JS/today.js
@@ -1,1 +1,17 @@
 // This is a JS file for the header section by Jim
+
+// Clock
+setInterval(tellTime, 1000);
+function tellTime() {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  let currentTime = hour + ":" + min + ":" + sec;
+
+  document.getElementById("clock").innerHTML = currentTime;
+}
