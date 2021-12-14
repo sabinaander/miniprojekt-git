fa0c4ae377b78847c2cdf53599c32a7208bb3570
Jim Bothén <jim.bothen@gmail.com>
2021-12-14 14:58:54 +0100

    Get date to header Fixes #38

--- JS/today.js
+++ JS/today.js
@@ -1,1 +1,11 @@
 // This is a JS file for the header section by Jim
+
+// Today's date
+
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+let today = year + "-" + month + "-" + day;
+
+document.getElementById("currentDate").innerHTML = today;
