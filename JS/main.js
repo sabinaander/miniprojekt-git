b9fca83d24251e440ff844bab013bc1fdee0979c
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2021-12-14 14:51:34 +0100

    fixed the toDo

--- JS/main.js
+++ JS/main.js
@@ -4,20 +4,10 @@ window.addEventListener('load', main)
  * @type {Array<{ title: String, description: String, date: String }>} asdas
  */
 let toDos = [
-  {
-    title: 'test todo1',
-    description: 'some text',
-    date: '2021-12-01',
-  },
-  {
-    title: 'test todo1',
-    description: 'some text',
-    date: '2021-12-13',
-  },
 ]
 
 function getToDosFromStorage(){
-  toDos = JSON.parse(localStorage.getItem('toDos'))
+  toDos = JSON.parse(localStorage.getItem('toDos'))||[]
 }
 
 // Starts the program
